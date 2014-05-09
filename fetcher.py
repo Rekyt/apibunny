9d8d58e0174de2247b5cbe50a0d76ba616b624c9
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-08 22:11:28 -0400

    Added Cell object to modelize cells

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+Module docstring.
+"""
 import urllib2
-response = urllib2.urlopen('')
-html = response.read()
+import json
+
+class Cell(object):
+	"""
+	Cell object of the maze.
+	"""
+	def __init__(self, json_dump):
+
+		self.number = ""
+		self.name = ""
+		self.url = ""
+		self.neighbors = {}
+		
+
+		self.number = str(json_dump["cells"][0]["id"])
+		self.url = "http://apibunny.com/cells/" + self.number
+		self.name = json_dump["cells"][0]["name"]
+		self.neighbors = json_dump["cells"][0]["links"]
+		del self.neighbors["maze"]
+
+	def __str__(self):
+
+		string = "Cell: \"{}\" ID: {}\nNeighbors:\n".format(self.name, self.number)
+		for key in self.neighbors.keys():
+			string += "Direction: {} ID: {}".format(key, self.neighbors[key])
+
+		return string
+
+
+start_url = urllib2.urlopen('http://apibunny.com/cells/taTKQ3Kn4KNnmwVI')
+
+start_cell = json.load(start_url)
